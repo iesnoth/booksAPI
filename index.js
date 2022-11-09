7b6e19d2a2f78ff02e0560e656dd82d7f7f64046
Victoria Beasley <iesnoth91@gmail.com>
2022-11-08 21:47:51 -0600

    cors added i think?

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 //configuration
 const app = express()
@@ -11,6 +12,7 @@ const PORT = process.env.PORT
 
 //middleware
 app.use(express.json())
+app.use(cors())
 
 //controller router
 app.use(`/books`, require(`./book-controller`))
@@ -20,9 +22,18 @@ app.get(`/`, (req, res) => {
     res.send('Hello world')
 })
 
+app.get('/products/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+  })
+   
+
 //configures mongoose/mongo
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true },
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
 //listen, keeps local server open
 app.listen(PORT)
+
+app.listen(80, function () {
+    console.log('CORS-enabled web server listening on port 80')
+  })
